d509a64d0c60df8c25eaba7da9ea57fcbb6964bb
Cameron <cameron.delc@gmail.com>
2022-02-13 13:55:42 -0800

    Update scripts.js

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,21 @@
+//able to hide controls
+//build own interface with a button for pause and play
+//volume slider
+//video speed
+//skip forward/back buttons
+
+/*Get Our Elements*/
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelector('[data-skip]');
+const ranges = player.querySelector('.player__slider');
+
+/*Build our Functions*/
+function togglePlay() {
+    //4min mark
+}
+
+/*Hook up Event Listeners*/
